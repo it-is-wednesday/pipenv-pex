bb003951f11b48f0759359627e0afabc6ef721d1
it-is-wednesday <maorkdosh@gmail.com>
2019-12-28 02:17:44 +0200

    init

---
+++ pipenv_pex.py
@@ -0,0 +1,98 @@
+import re
+import sys
+import tempfile
+from pathlib import Path
+from typing import List
+
+import click as c
+from pex.bin.pex import main as pex_main
+from pipenv.project import Project  # type: ignore
+
+FILES_IRRELEVANT_TO_PEX = [
+    "Pipfile", "Pipfile.lock", ".mypy_cache", "__pycache__"
+]
+
+
+class StashAwayFiles:
+    """
+    Context manager for temporarily moving away certain file in a directory.
+    On exit, the files are moved back to their original home <3
+
+        >>> from pathlib import Path
+        >>> dir = Path("test")
+        >>> dir.mkdir(exist_ok=True)
+        >>> (dir / "in").touch()
+        >>> (dir / "bad").touch()
+        >>> with StashAwayFiles(dir, ["bad"]):
+        ...     print([x for x in dir.iterdir()])
+        ...
+        [PosixPath('test/in')]
+        >>> print([x for x in dir.iterdir()])
+        [PosixPath('test/in'), PosixPath('test/bad')]
+
+    """
+    def __init__(self, origin, predicate: List[str]):
+        self.predicate = predicate
+        self.origin = Path(origin)
+
+    def __enter__(self):
+        # using home dir instead of /tmp becuase it's impossible to truly move
+        # files between different filesystems - moving a file pointer around is
+        # only possible within the same filesystem. since all we want is to
+        # hide these files for a moment, meaning moving a pointer, we'll have
+        # to pick a directory within the same filesystem we're currently on.
+        self.tmpdir = tempfile.TemporaryDirectory(dir=str(Path.home()))
+        for f in self.origin.iterdir():
+            if f.name in self.predicate:
+                f.rename(f"{self.tmpdir.name}/{f.name}")
+
+    def __exit__(self, type, value, traceback):
+        for f in Path(self.tmpdir.name).iterdir():
+            f.rename(f"{self.origin}/{f.name}")
+
+
+@c.command(context_settings={"ignore_unknown_options": True})
+@c.option("-e", "--entry-point")
+@c.option("-x",
+          "--exclude",
+          multiple=True,
+          help="Don't include these files/directories in the resulting .pex "
+          "file. In addition to files/dirs added using this option, these are "
+          "excluded by deafult:\n{}.".format(
+              ", ".join(FILES_IRRELEVANT_TO_PEX)))
+@c.argument("pex_args", nargs=-1, type=c.UNPROCESSED)
+def main(entry_point, exclude: List[str], pex_args):
+    project = Project()
+    proj_dir = project.project_directory
+
+    if main and re.match(r"(.*\.)?.*:.*", entry_point) is None:
+        print(
+            "Entry point doesn't conform to template "
+            "'package.module:function'",
+            file=sys.stderr)
+        return
+
+    if not (main or Path(f"{proj_dir}/__main__.py").exists()):
+        print(
+            "Neither an entry point was given nor a __main__.py "
+            "file exists. I need something to work with!",
+            file=sys.stderr)
+        return
+
+    # add inferred output filename if none were found in pex params
+    if not ("-o" in pex_args or "--output" in pex_args):
+        output = f"{proj_dir}/{project.name}.pex"
+        pex_args += ("--output", output)
+
+    deps = [
+        f"{key}{value['version']}"
+        for key, value in project.get_or_create_lockfile()["default"].items()
+    ]
+
+    irrelevant = FILES_IRRELEVANT_TO_PEX + list(exclude)
+    with StashAwayFiles(proj_dir, irrelevant):
+        pex_main([*deps, "--sources-directory", proj_dir, *pex_args])
+
+
+if __name__ == '__main__':
+    main()
