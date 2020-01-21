310a20e1e2ce448d0a0da168f090ebebc6468656
it-is-wednesday <maorkdosh@gmail.com>
2020-01-21 22:43:10 +0200

    add entry point and dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pipenv-pex",
-    version="0.1",
+    version="0.2",
     author="Maor Kadosh",
     description="Generate Python executable files via PEX using info from Pipfile",
     long_description=long_description,
@@ -17,5 +17,16 @@ setuptools.setup(
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        "pipenv",
+        "pex",
+        "click",
+        "colorama"
+    ],
+    entry_points="""
+        [console_scripts]
+        pipenv-pex=pipenv_pex.pipenv_pex:main
+    """,
+    include_package_data=True,
     python_requires='>=3.7',
 )
